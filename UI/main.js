64f70d7dafd1c7a34b3a82549d469ba4a798e62a
TheSteelGuy <collotuos0@gmail.com>
2018-07-04 16:22:22 +0300

    add signup functionality to Ui templates using javascript

--- UI/main.js
+++ UI/main.js
@@ -1,4 +1,23 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+function populate() {
+	function reqListener() {
+		if (this.readyState == 4 && this.status == 200) {
+			let obj = JSON.parse(this.responseText);
+			document.getElementById('stop').innerHTML = obj.ride.destination
+			document.getElementById('departure').innerHTML = obj.ride.departure
+			document.getElementById('date').innerHTML = obj.ride.date
+			document.getElementById('charges').innerHTML = obj.ride.charges
+			document.getElementById('meet').innerHTML = obj.ride.meetpoint
 
-function details(){
-    alert('Destination:Thika| Fees:400| Meeting point:Rundah| Dep:9.00am')
+		};
+
+	}
+
+	var oReq = new XMLHttpRequest();
+	oReq.addEventListener("load", reqListener);
+	oReq.open("GET", "https://mighty-beach-21248.herokuapp.com/api/v1/rides/1", true);
+	oReq.send();
 }
+
+
+
